7ec8d65037662540bc379b2f2f552d0d1b03865d
Zhaz <Zhazgulp@gmail.com>
2019-08-11 19:49:29 -0400

    new line

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator {
     public static double add(double num1,num2){
         return num1+num2;
+
+
     }
-}
+    public static double substract(double num1,double num2){
+        return num1-num2;
+    }
+
