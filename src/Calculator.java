0f7825aae6fcba5356d150a6f36d4f40d6aef638
Zhaz <Zhazgulp@gmail.com>
2019-08-11 19:58:58 -0400

    substarct method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public static double add(double num1,num2){
         return num1+num2;
 
-
+//vjdhvsjdhvsjhdvj
     }
     public static double substract(double num1,double num2){
         return num1-num2;
