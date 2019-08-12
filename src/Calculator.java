f20da641653b2d895c20cab1da6f9727bca7654a
Zhaz <Zhazgulp@gmail.com>
2019-08-11 21:57:10 -0400

    new method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,6 +7,11 @@ public class Calculator {
     public static double substract(double num1,double num2){
         return num1-num2;
     }
-    public static double multiply(double num1,double num2){
-        return num1*num2;
+    public static double multiply(double num1,double num2) {
+        return num1 * num2;
 
+    }
+    public void method1(String name){
+
+    }
+}
