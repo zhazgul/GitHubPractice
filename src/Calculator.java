ee414ec72b93e91e2c280121c2a110714b67dcfc
zhazgul <30359115+zhazgul@users.noreply.github.com>
2019-08-11 20:11:33 -0400

    add multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,6 @@ public class Calculator {
     public static double substract(double num1,double num2){
         return num1-num2;
     }
+    public static double multiply(double num1,double num2){
+        return num1*num2;
 
